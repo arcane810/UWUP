3406ea740d988633e243294f8af82b584b462898
nubular <nubular2001@gmail.com>
2021-04-03 03:52:28 +0530

    Packing/unpacking packets

--- protocol/Packet.cpp
+++ protocol/Packet.cpp
@@ -5,5 +5,30 @@
 Packet::Packet(char *buffer, int len) {
     packet_struct = (PacketStruct *)malloc(len * sizeof(char));
     memcpy(packet_struct, buffer, len * sizeof(char));
-    // TODO unpack header
+    // packet_struct = (PacketStruct *)buffer;
+    ack_number = packet_struct->ack_number;
+    seq_number = packet_struct->seq_number;
+    flags = packet_struct->flags;
+    rwnd = packet_struct->rwnd;
+    // should I malloc this?
+    // data = (char *)malloc(len * sizeof(char) - 13);
+    // memcpy(data, packet_struct->data,len * sizeof(char) - 13);
+}
+
+Packet::Packet(uint32_t ack_number, uint32_t seq_number, uint32_t flags, uint32_t rwnd, char* buffer, int len) : ack_number(ack_number), seq_number(seq_number), flags(flags), rwnd(rwnd) {
+    packet_struct = (PacketStruct *) malloc(13 + sizeof(char) * len);
+    packet_struct->ack_number = ack_number;
+    packet_struct->seq_number = seq_number;
+    packet_struct->flags = flags;
+    packet_struct->rwnd = rwnd;
+    memcpy(packet_struct->data, buffer, len * sizeof(char));
+    // data = (char *)malloc(len * sizeof(char) - 13);
+    // memcpy(data, packet_struct->data,len * sizeof(char) - 13);
+}
+
+
+std::ostream& operator<<(std::ostream& os, Packet const& p)
+{
+    os << "{\n" << p.ack_number << ' ' << p.seq_number <<  ' ' << p.rwnd << '\n' << p.packet_struct->data << "\n}" << std::endl;
+    return os;
 }
--- protocol/Packet.hpp
+++ protocol/Packet.hpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <iostream>
 
 /**
  * A structure to store the packed structure
@@ -25,6 +26,18 @@ class Packet {
     PacketStruct *packet_struct;
     /// Total length of packet (length of data + length of header)
     int packet_length;
+    /// ACK Number ( 4 Bytes )
+    uint32_t ack_number;
+    /// Seq Number ( 4 Bytes )
+    uint32_t seq_number;
+    /// Flags ( 1 Byte )
+    uint8_t flags;
+    /// Advertised Receive Window ( 4 Bytes )
+    uint32_t rwnd;
+    /// Data recvd
+    // char *data;
 
     Packet(char *buffer, int len);
+    Packet(uint32_t ack_number, uint32_t seq_number, uint32_t flags, uint32_t rwnd, char* buffer, int len);
+    friend std::ostream& operator<<(std::ostream& os, Packet const& p);
 };
